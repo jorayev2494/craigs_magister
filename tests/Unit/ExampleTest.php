9df988494faa3d6ddb95dbd61e35bf9455b6b9f1
jorayev2494 <jorayev2494@gmail.com>
2020-09-25 01:34:05 +0300

    first commit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    private string $text;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+    
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+        User::factory()->count(1)->make();
+    }
+
+    public function testThisIsMyTest()
+    {
+        $this->assertEquals('test', 'test');
+        $this->assertSame('1', '1');        
+    }
+    
+}
